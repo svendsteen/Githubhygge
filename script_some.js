70d085ee0f5b532e91bd9623ff535d965ca8987f
Zennuba <loni70540@edu.ucl.dk>
2025-09-02 11:22:04 +0200

    Update

--- script_some.js
+++ script_some.js
@@ -1,4 +1,4 @@
-const socialLinks = document.querySelectorAll("footer__section footer__section--socials a[target='_blank']");
+const socialLinks = document.querySelectorAll("footer__section footersection__socials");
 socialLinks.forEach(link => {
   link.addEventListener("click", () => {
     console.log("Bruger klikkede på:", link.href);
